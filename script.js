a6b5f387c9cd245acaccb13471cd24093d096484
Thomson1511 <kovacstomi130@gmail.com>
2024-11-10 18:21:46 +0100

    added points to js

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+let PointsList = ["gasna", "begla", "pesat", "abeti", "sopro", "arsin", "stein", "sasal", "sunis", "gotar", "dimlo", "nekin", "kopry", "vebal",
+     "osduk", "bareb", "babit", "tondo", "kerop", "parak", "inved", "mopug", "tegri", "deget", "budop", "megik", "narka", "bador", "karil", "gemto", "lonla", "kenin", "keked",
+     "abuli", "pitok", "demop", "edemu", "litku", "amrax", "balap", "patak", "badov", "ergom", "alamu", "xomba", "vamog", "kuvex", "natex", "tekno", "zolku", "ogvun", "subes",
+     "sirdu", "gitas", "olati", "nalox", "nohat", "sunor", "nikab", "sogmo", "okora", "pidon", "mavir", "eboro", "verig", "perit", "kusis", "romka", "sag-vor", "kovek", "rigsa",
+     "ibliz", "mizol", "boksi", "oslen", "binku", "uvera", "bks-vor", "bug-vor", "svr-vor", "ptb-vor", "gyr-vor", "mnr-vor", "bud-vor", "tps-vor", "gilep", "vajdi", "ulzak",
+     "duzla", "fahaz", "gazda", "ebamo", "kezal", "vetik", "beted", "witri", "gelka", "bodza", "torno", "pucog", "vebos", "rakfa", "jozep", "pusta", "epari", "etaro", "balux",
+     "ilhak", "dodar", "babox", "erguz", "abony", "zurfa", "norah", "nipur", "lahor"];
+
+
 const zoomableElement = document.getElementById('zoomable');
 let isZoomed = false;
 
@@ -11,6 +20,9 @@ document.addEventListener('mousedown', (e) => {
         } else {
             zoomOut();
         }
+    }
+    else if (e.button === 4) { // Fourth button (usually back button)
+        alert(`Position - Top: ${e.clientY - 5}px, Left: ${e.clientX - 5}px`);
     }
 });
 
